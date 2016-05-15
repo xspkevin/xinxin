2d66209858fb242e5a49a72e3cc732ebd2cf1cb2
徐升平 <xspkevin@163.com>
2016-05-15 14:45:04 +0800

    初始化代码

---
+++ source/ios/xinxin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xinxin
+//
+//  Created by 徐升平 on 16/5/15.
+//  Copyright © 2016年 Xu Shengping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ source/ios/xinxin/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  xinxin
+//
+//  Created by 徐升平 on 16/5/15.
+//  Copyright © 2016年 Xu Shengping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
